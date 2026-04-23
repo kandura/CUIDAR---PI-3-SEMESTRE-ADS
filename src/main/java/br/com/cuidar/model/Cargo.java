73fca37143c099fae49b22de5fd442c1aab91c76
Leonardo Souza <81191275+kandura@users.noreply.github.com>
2026-04-23 16:39:52 -0300

    Algumas das classes comentadas para javadoc

---
+++ src/main/java/br/com/cuidar/model/Cargo.java
@@ -0,0 +1,10 @@
+package br.com.cuidar.model;
+
+/**
+ * Enumera os cargos disponíveis para um {@link Funcionario} na ILPI.
+ */
+public enum Cargo {
+    ADMINISTRADOR,
+    ENFERMEIRO,
+    CUIDADOR
+}
